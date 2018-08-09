03cd6e73d917dc04264151ec2580d5df6289f8dc
Jefta Dirksen <jeftadirksen@gmail.com>
2018-08-09 14:13:35 +0200

    Init

---
+++ AutoUpdate.config.example.ps1
@@ -0,0 +1,12 @@
+﻿# Config
+$minecraftServerDir = "..\server"	# Location of Mincecraft server folder
+$versionType = "release"			# Type of version to check for (release/snapshot)
+$taskName = "Minecraft Server"		# The name of the scheduled task which runs the Minecraft server
+$logFile = "AutoUpdate.log"			# Logfile filename
+
+# E-mail config
+$EmailFrom = "admin@minecraftserver.com"
+$EmailTo = "my@gmail.com"
+$SMTPServer = "smtp.gmail.com"
+$SMTPUser = "my@gmail.com"
+$SMTPPass = "********"
---
+++ AutoUpdate.ps1
@@ -0,0 +1,74 @@
+﻿# Include config
+. "$PSScriptRoot\AutoUpdate.config.ps1"
+
+# Mojang urls
+$versionsJsonUrl = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+$versionsJson = Invoke-RestMethod $versionsJsonUrl
+
+function Main {
+	try {
+		# Resolve latest version
+		$version = $versionsJson.latest.$versionType
+		# Compose filename
+		$fileName = "minecraft_server.$version.jar"
+		
+		# Script finished if file already exists
+		If (Test-Path $fileName) {
+			Write-Log "No newer version available $version"
+			exit
+		}
+		Else {
+			Write-Log "New version available $version"
+		}
+
+		# Get download URL
+		$versionsJsonVersion = $versionsJson.versions | Where-Object {$_.id -eq $version}
+        $versionJsonUrl = $versionsJsonVersion.url
+        $versionJson = Invoke-RestMethod $versionJsonUrl
+        $downloadUrl = $versionJson.downloads.server.url
+
+        # Download
+		Write-Log "Downloading $fileName"
+		Invoke-WebRequest $downloadUrl -OutFile $fileName
+
+		# Stop server
+		Write-Log "Stopping server"
+		Stop-ScheduledTask -TaskName $taskName
+
+		# Backup
+        $timestamp = Get-Date -Format o | foreach {$_ -replace ":","."}
+		$archive = $PSScriptRoot + "\backup-pre-" + $version + "-update-" + $timestamp + ".zip"
+		Write-Log "Creating backup $archive"
+		Compress-Archive -Path $minecraftServerDir -DestinationPath $archive -Force
+
+		# Update
+		Write-Log "Updating server.jar"
+		Copy-Item $fileName "$minecraftServerDir\server.jar"
+		
+		# Start server
+		Write-Log "Starting server"
+		Start-ScheduledTask -TaskName $taskName
+		
+		# Send e-mail
+		Write-Log "Sending E-mail"
+		$Subject = "$taskName updated to $versionType version $version"
+		$Body = "$taskName has been updated to minecraft $versionType version $version"
+		$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+		$SMTPClient.EnableSsl = $true
+		$SMTPClient.Credentials = New-Object System.Net.NetworkCredential($SMTPUser, $SMTPPass)
+		$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+	}
+	catch {
+		$msg = $_.Exception.Message
+		Write-Log "Error: $msg"
+		exit
+	}
+}
+
+function Write-Log {
+    $date = (Get-Date).ToShortDateString()
+    $time = (Get-Date).ToShortTimeString()
+    Add-Content $logFile "$date $time $($args[0])"
+}
+
+Main
